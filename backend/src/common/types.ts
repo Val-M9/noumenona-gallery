063dc475edbb56d5c6ee86ed9ba997e410335f27
Valera <v.mischenko90@gmail.com>
2026-08-08 21:17:02 +0300

    chore: moove types to proper folder

--- backend/src/common/types.ts
+++ backend/src/common/types.ts
@@ -1,1 +1,5 @@
 export type IdParams = { Params: { id: string } };
+
+export type ArtworkImageParams = { Params: { artworkId: string } };
+
+export type ArtworkImageDetailParams = { Params: { artworkId: string; imageId: string } };
